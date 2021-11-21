4f272ce9c1165f290f2e05baf07908a9181be9b7
BlackJX <profesorhiro@gmail.com>
2021-11-21 19:12:53 +0300

    4.2. Заголовок в файле java [#159036]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -1,7 +1,6 @@
 package ru.job4j;
 
 public class Info {
-    public static void main(String[] args) {
-        System.out.println("21.11.2021");
+    public static void main(String[] args) { System.out.println("21.11.2021");
     }
 }
