f2d05cbcdf9c4246116be18bacd4363e230d2f86
BlackJX <profesorhiro@gmail.com>
2021-11-25 23:13:18 +0300

    3.1. Переназначение переменной [#232354]

--- src/ru/job4j/Calculator.java
+++ src/ru/job4j/Calculator.java
@@ -3,7 +3,7 @@ package src.ru.job4j;
 public class Calculator {
     public static void main(String[] args) {
         int size = 10;
-        size = size + 90;
+        size = 100;
         size = size - 5;
         System.out.println(size);
 
