095ccea4e2f9f8e105b509683c321733bb374523
BlackJX <profesorhiro@gmail.com>
2022-02-02 10:04:25 +0300

    5. Конвертер валюты [#41583 #233496]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -3,7 +3,7 @@ package src.ru.job4j.converter;
 public class Converter {
 
     public static float rubleToEuro(float value) {
-        float rsl = value / 70; /
+        float rsl = value / 70;
         return rsl;
     }
 
