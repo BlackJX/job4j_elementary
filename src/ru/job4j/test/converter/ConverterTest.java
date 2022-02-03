d797fa981392229b7e434c99f1ea27a46b117ab1
BlackJX <profesorhiro@gmail.com>
2022-02-03 17:33:17 +0300

    1. Модульные тесты. [#159435]

---
+++ src/ru/job4j/test/converter/ConverterTest.java
@@ -0,0 +1,14 @@
+package src.ru.job4j.test.converter;
+
+import static org.junit.Assert.*;
+
+public class ConverterTest {
+
+    @org.junit.Test
+    public void rubleToEuro() {
+    }
+
+    @org.junit.Test
+    public void rubleToDollar() {
+    }
+}
