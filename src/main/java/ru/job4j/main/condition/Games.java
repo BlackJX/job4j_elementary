a8591d889ee84871ac025d5af59c42a048f5ee46
BlackJX <profesorhiro@gmail.com>
2022-02-09 21:54:32 +0300

    4.3.1. Оператор && [#333783]

---
+++ src/main/java/ru/job4j/main/condition/Games.java
@@ -0,0 +1,18 @@
+package src.main.java.ru.job4j.main.condition;
+
+public class Games {
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        if (allowByParent && hasMoney) {
+            System.out.println("I can go to he computer club.");
+        } else {
+            System.out.println("I cant.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Games.permission(true, true);
+        Games.permission(true, false);
+        Games.permission(false, false);
+        Games.permission(false, false);
+            }
+}
