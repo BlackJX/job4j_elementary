a2b2e84747d200bdc56218f127715100e54601e7
BlackJX <profesorhiro@gmail.com>
2022-03-02 11:11:43 +0300

    4.2. Тернарное сравнение [#189 #262665]

--- src/main/java/ru/job4j/condition/Triangle.java
+++ src/main/java/ru/job4j/condition/Triangle.java
@@ -3,10 +3,15 @@ package src.main.java.ru.job4j.condition;
 public class Triangle {
 
     public static boolean exist(double ab, double ac, double bc) {
-        return ((ab + ac) > bc) && ((ab + bc) > ac);
+        return ((ab + ac) > bc) && ((ab + bc) > ac) && ((ac + bc) > ab);
     }
 
     public static void main(String[] args) {
-        System.out.println(exist(2.0, 2.0 , 2.0));
+        System.out.println(exist(2.0, 2.0, 2.0));
+        if (exist(2.0, 2.0, 2.0)) {
+        System.exit(0);
+    } else {
+        System.exit(1);
+        }
     }
 }
