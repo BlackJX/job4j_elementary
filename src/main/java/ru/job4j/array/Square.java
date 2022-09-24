8c60ab9dc00074b9156e5b39cdb3168dc238a36e
BlackJX <profesorhiro@gmail.com>
2022-09-24 23:37:31 +0300

    6.0.7. Массивы в тестах. [#226789 #287178]

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -1,16 +1,17 @@
 package ru.job4j.array;
+public class Square {
+    public static int[] calculate(int bound) {
+        int[] rst = new int[bound];
+        for (int i = 1; i < rst.length; i++) {
+            rst[i] = i * i;
+        }
+        return rst;
+    }
 
-public class Sequence {
     public static void main(String[] args) {
-        int[][] array = new int[3][3];
-        array[0][0] = 1;
-        array[0][1] = 2;
-        array[0][2] = 3;
-        array[1][0] = 4;
-        array[1][1] = 5;
-        array[1][2] = 6;
-        array[2][0] = 7;
-        array[2][1] = 8;
-        array[2][2] = 9;
+        int[] array = calculate(4);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 }
--- src/test/java/ru/job4j/array/SquareTest.java
+++ src/test/java/ru/job4j/array/SquareTest.java
@@ -1,8 +1,6 @@
 package ru.job4j.array;
 
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SquareTest {
@@ -10,7 +8,7 @@ public class SquareTest {
     @Test
     public void whenBound3Then014() {
         int bound = 3;
-        int[] result = Sequence.calculate(bound);
+        int[] result = Square.calculate(bound);
         int[] expected = new int[] {0, 1, 4};
         assertThat(result).containsExactly(expected);
     }
@@ -18,7 +16,7 @@ public class SquareTest {
     @Test
     public void whenBound5Then01416() {
         int bound = 5;
-        int[] result = Sequence.calculate(bound);
+        int[] result = Square.calculate(bound);
         int[] expected = new int[] {0, 1, 4, 9, 16};
         assertThat(result).containsExactly(expected);
     }
